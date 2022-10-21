42d6b480d392e1dde4bf68c0c685d6011c434dfb
sa_jamali <R57c%lwkNU#C^Z>
2022-10-21 20:43:17 +0330

    retrofit impl

---
+++ app/src/main/java/com/itoll/githubusers/data/remote/ApiClient.kt
@@ -0,0 +1,65 @@
+package com.itoll.githubusers.data.remote
+
+import com.google.gson.GsonBuilder
+import com.itoll.githubusers.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+class ApiClient {
+    companion object {
+        private const val API_BASE_URL = BuildConfig.API_URL
+        private const val CONNECTION_TIME_OUT = 10L
+        private const val READ_TIME_OUT = 10L
+        private const val WRITE_TIME_OUT = 10L
+
+        private var baseRetrofit: Retrofit? = null
+        private var gson = GsonBuilder()
+            .setLenient()
+            .serializeNulls()
+            .create()
+
+        @Synchronized
+        fun getRetrofitInstance(
+        ): Retrofit = baseRetrofit?.run {
+            this
+        } ?: run {
+            Retrofit.Builder()
+                .baseUrl(API_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(getBaseOkHttpClient())
+                .build().apply {
+                    baseRetrofit = this
+                }
+
+        }
+
+        @Synchronized
+        fun getBaseOkHttpClient()
+                : OkHttpClient = OkHttpClient().newBuilder().addInterceptor {
+            val originalRequest: Request = it.request()
+            val builder = originalRequest.newBuilder().addHeader(
+                "Content-Type", "application/json"
+            ).addHeader("X-FEATURE-VERSION", "1")
+                .addHeader("Authorization", "Bearer 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")
+
+            val newRequest = builder.build()
+            it.proceed(newRequest)
+        }.connectTimeout(CONNECTION_TIME_OUT, TimeUnit.SECONDS)
+            .writeTimeout(WRITE_TIME_OUT, TimeUnit.SECONDS)
+            .readTimeout(READ_TIME_OUT, TimeUnit.SECONDS).apply {
+                if (BuildConfig.DEBUG) {
+                    val httpLogInterceptor = HttpLoggingInterceptor()
+                    httpLogInterceptor.level = HttpLoggingInterceptor.Level.BODY
+                    addInterceptor(httpLogInterceptor)
+                }
+            }.run {
+                build()
+            }
+
+    }
+
+}
