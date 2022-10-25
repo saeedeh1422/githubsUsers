e1b5230fd2d249f680c8f3e49c3ffaf8c3bf017a
sa_jamali <R57c%lwkNU#C^Z>
2022-10-25 23:22:33 +0330

    add resources

--- app/src/main/java/com/itoll/githubusers/presentation/MainActivity.kt
+++ app/src/main/java/com/itoll/githubusers/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.itoll.githubusers
+package com.itoll.githubusers.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.itoll.githubusers.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
